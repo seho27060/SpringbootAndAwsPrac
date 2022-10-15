b0ef200c008a38bffea43d90381fc1b61e0beda4
seho <seho27060@gmail.com>
2022-10-15 14:52:40 +0900

    CH.3 address DB to use spring JPA

--- src/main/java/com/springAndAWSprac/springboot/service/posts/PostsService.java
+++ src/main/java/com/springAndAWSprac/springboot/service/posts/PostsService.java
@@ -1,9 +1,14 @@
 package com.springAndAWSprac.springboot.service.posts;
 
+import com.springAndAWSprac.springboot.domain.posts.Posts;
 import com.springAndAWSprac.springboot.domain.posts.PostsRepository;
+import com.springAndAWSprac.springboot.web.dto.PostsResponseDto;
 import com.springAndAWSprac.springboot.web.dto.PostsSaveRequestDto;
+import com.springAndAWSprac.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -11,5 +16,20 @@ public class PostsService {
     private final PostsRepository postsRepository;
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("there is no content matched with id="+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("there is no content matched with id="+id));
+
+        return new PostsResponseDto(entity);
     }
 }
